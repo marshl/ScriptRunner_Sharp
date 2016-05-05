a3cd2890a02642a9b4e16e947d3c6fa423775607
marshl <marshl@live.com.au>
2016-05-05 21:06:45 +1000

    Converted ExRuntimeRoot to C#;

--- ScriptRunner_Sharp/ex/ExRuntimeRoot.cs
+++ ScriptRunner_Sharp/ex/ExRuntimeRoot.cs
@@ -1,33 +1,27 @@
-package com.fivium.scriptrunner2.ex;
 
-/**
- * Root runtime exception class for unchecked exceptions.
- */
-public class ExRuntimeRoot 
-extends RuntimeException {
-  
-  private void logThis(){
-//    Logger.logInfo("ERROR:::");
-//    Logger.logError(this);
-  }
-  
-  public ExRuntimeRoot(Throwable pThrowable) {
-    super(pThrowable);
-    logThis();
-  }
 
-  public ExRuntimeRoot(String pString, Throwable pThrowable) {
-    super(pString, pThrowable);
-    logThis();
-  }
+namespace ScriptRunner_Sharp
+{
+    using System;
+    /**
+     * Root runtime exception class for unchecked exceptions.
+     */
+    public class ExRuntimeRoot : Exception
+    {
+        public ExRuntimeRoot(string pString, Exception pThrowable) : base(pString, pThrowable)
+        {
 
-  public ExRuntimeRoot(String pString) {
-    super(pString);
-    logThis();
-  }
+        }
 
-  public ExRuntimeRoot() {
-    super();
-    logThis();
-  }
+        public ExRuntimeRoot(string pString) : base(pString)
+        {
+
+        }
+
+        public ExRuntimeRoot() : base()
+        {
+
+        }
+    }
+
 }
